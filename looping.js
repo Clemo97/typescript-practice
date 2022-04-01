3679154a1b450dc7b1a027a4a4be4c11da733d3e
Clemo97 <lumumbaclement@gmail.com>
2022-04-01 10:11:01 +0300

    typescript file about looping

---
+++ looping.js
@@ -0,0 +1,11 @@
+var person1 = { "first": "James", "last": "Muriuki" };
+for (var prop in person1) {
+    console.log(prop);
+}
+//first
+//last
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
+    var num = numbers_1[_i];
+    console.log(num);
+}
---
+++ looping.ts
@@ -0,0 +1,10 @@
+const person1 = {"first":"James","last":"Muriuki"};
+for(let prop in person1){
+    console.log(prop)
+}
+//first
+//last
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+for (let num of numbers){
+    console.log(num);
+}
