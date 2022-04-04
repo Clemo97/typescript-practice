128117b78c478c08ae9f48460ffd6325d886a502
Clemo97 <lumumbaclement@gmail.com>
2022-04-04 07:46:45 +0300

    Arrays and tuples

--- looping.ts
+++ looping.ts
@@ -8,3 +8,4 @@ const numbers=[1,2,3,4,5,6,7,8,9,10]
 for (let num of numbers){
     console.log(num);
 }
+
---
+++ types.js
@@ -0,0 +1,8 @@
+//Declaring and initialising a typescript array
+var multiline = [23, "red", 45, "hello"];
+console.log(multiline);
+//Declaring and initialising of a Tuple, when you don't want typechecking
+var someTuple;
+//initialise it
+someTuple = ["Clement", 1997]; //OK
+console.log(someTuple);
